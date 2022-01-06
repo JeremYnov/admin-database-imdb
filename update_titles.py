6de1d50765a8d78085f32c67536e76c78ee5b46e
loux22 <louis.ardilly@yahoo.fr>
2022-01-06 15:09:02 +0100

    [edit] suppression commentaire

--- update_titles.py
+++ update_titles.py
@@ -14,13 +14,8 @@ with open('csv/title-ratings/title-ratings.tsv') as title_ratings:
     with alive_bar(nbLines) as bar:
         for count, row in enumerate(csv_reader): 
             if count != 0:
-                print(count)
                 requests.append(UpdateOne({'tconst' : row[0]}, {'$set' : {'averageRating':float(row[1]), 'numVotes':int(row[2])}}))
-                if count % 10000 == 0:
-                    print("Len of REQUESTS :" + str(len(requests)))
-                    print("============================================TEST 1 ===============================")
+                if count % 100000 == 0:
                     bulk = prod_db.bulk_write(requests)
-                    print("============================================TEST 2 ===============================")
                     requests = []
-                    print(bulk.bulk_api_result)
             bar()             
