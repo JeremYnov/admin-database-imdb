78be5b549b53ea49eb648221744da091a2c466ef
loux22 <louis.ardilly@yahoo.fr>
2022-01-06 15:08:38 +0100

    [add] creation index

---
+++ CreateIndex.py
@@ -0,0 +1,13 @@
+import db_config
+
+
+# creation des collections
+collection_production = db_config.database['production']
+
+# récuperation colletion
+prod_db = db_config.database.get_collection("production")
+
+
+# DECOMMENTER POUR CREER L'INDEXE
+# prod_db.create_index('cuisine')
+print(prod_db.index_information())
